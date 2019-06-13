8f600dad303cd08104d7aba5353ca66d1bfdb8ad
new <new@DESKTOP-GMJ0S4E>
2019-06-13 12:28:23 +0530

    Spring Boot Data Jpa All Operations(save, saveAll,deleteById,deleteAll,deleteAllInBatch,findById,findByAll)

--- src/main/java/com/app/runner/ConsoleRunner.java
+++ src/main/java/com/app/runner/ConsoleRunner.java
@@ -1,5 +1,9 @@
 package com.app.runner;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,9 +17,46 @@ public class ConsoleRunner implements CommandLineRunner {
 	private ProductRepository repo;
 	@Override
 	public void run(String... args) throws Exception {
-		repo.save(new Product(1,"A",1.2));
-		repo.save(new Product(2,"B",2.2));
+		/*repo.save(new Product(6,"F",6.2));
+		repo.save(new Product(2,"B",2.2));*/
 		
+		//--update call--
+		
+		//repo.save(new Product(2,"B",3.2));
+		
+		//--Bulk Insert
+		
+		/*List<Product> prds= Arrays.asList(
+				      new Product(3,"C",3.2),
+				      new Product(4,"D",4.2),
+				      new Product(5,"E",5.2)
+				      );
+		repo.saveAll(prds);*/
+		
+		//fetch one row
+		
+		/*Optional<Product> p=repo.findById(2);
+		if(p.isPresent()) {
+			Product prd = p.get();
+			System.out.println("Product Code is:"+prd.getProdCode());
+		}else {
+			System.out.println("Row not found");
+		}*/
+		
+		//--fetch all rows
+		
+		/*List<Product> list=repo.findAll();
+		list.forEach(System.out::println);*/
+		
+		//--- Delete Operation
+		
+		/*repo.deleteById(6);*/
+		
+		//--Delete All Rows
+		
+		/*repo.deleteAll();*/
+		
+		repo.deleteAllInBatch();
 	}
 
 }
